6b4be0587589ef45874a518308b42b9a9ee7a954
tonyx <tonyx@yahoo.com>
2010-03-18 23:41:01 +0100

    added test

---
+++ LabelManager/LabelManagerTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace LabelManager
+{
+    [TestFixture]
+    class LabelManagerTest
+    {
+
+        [Test]
+        public void TheNaturalMockOfSingletonLabelManagerContainsTwoElements()
+        {
+            Assert.AreEqual(2,SingletonLabelManager.getInstance().GetKeyCollection().Count);
+        }
+
+        [Test]
+        public void TheAttributeIsChangedByReflection()
+        {
+            Sample sample = new Sample("");
+            sample.Attribute = "string";
+            Assert.AreEqual("string",sample.Attribute);
+            LabelUtils.UpdateUi(sample);
+            Assert.AreEqual("valore italiano", sample.Attribute);
+        }
+    }
+    class Sample
+    {
+        private string privateField;
+        public string PublicField;
+        private string _attribute;
+
+        public Sample(string privateField)
+        {
+            this.privateField = privateField;
+        }
+
+        public string Attribute
+        {
+            get { return _attribute; }
+            set { _attribute = value; }
+        }
+    }
+}
